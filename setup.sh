5863f002df260e082cbbb46fcfafa52f4b47ee06
reyniersbram <reyniersbram@gmail.com>
2022-12-19 20:29:26 +0100

    clone spicetify themes

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 
+# link nvim config to this repo
 ln -s ~/dotfiles/nvim/ ~/.config/
+
+# clone spicetify config
+git clone git@github.com:reyniersbram/spicetify-themes.git ~/.config/spicetify/Themes/
 
