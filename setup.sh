4c1378a2c49f979b9b7b66fadc4cf4e25a2fa0ec
reyniersbram <55666730+reyniersbram@users.noreply.github.com>
2022-12-28 11:03:35 +0100

    modified setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 
-# link nvim config to this repo
-ln -s ~/dotfiles/nvim/ ~/.config/
+# The directory of this repository
+CDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # clone spicetify config
 git clone git@github.com:reyniersbram/spicetify-themes.git ~/.config/spicetify/Themes/
+
+# stow the correct files into the home directory
+stow --ignore 'LICENSE|.*\.md|setup.sh' --target $HOME
 
