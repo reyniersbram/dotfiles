0213decb521b64031bfb6b901345816f06b997d4
reyniersbram <55666730+reyniersbram@users.noreply.github.com>
2022-12-28 15:26:10 +0100

    fix setup, add alacritty config

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ CDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # clone spicetify config
 git clone git@github.com:reyniersbram/spicetify-themes.git ~/.config/spicetify/Themes/
 
+mkdir -p $HOME/.config/
 # stow the correct files into the home directory
-stow . --ignore 'LICENSE|.*\.md|setup.sh' --target $HOME
+stow . --ignore 'setup.sh' --target $HOME
 
