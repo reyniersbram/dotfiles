043ef9130d7e8396db7cc19b4307fd84d118bb08
reyniersbram <55666730+reyniersbram@users.noreply.github.com>
2023-01-14 22:41:58 +0100

    begin on font pulling

---
+++ setup/fonts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts/Hack
+
+# Normal font
+wget https://github.com/ryanoasis/nerd-fonts/blob/6f0b1fbf4fb03f58015d587c64c84a1e4e38e8d8/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf
+
+# Bold font
+wget https://github.com/ryanoasis/nerd-fonts/blob/6f0b1fbf4fb03f58015d587c64c84a1e4e38e8d8/patched-fonts/Hack/Bold/complete/Hack%20Bold%20Nerd%20Font%20Complete.ttf
+
+# Italic font
+wget https://github.com/ryanoasis/nerd-fonts/blob/6f0b1fbf4fb03f58015d587c64c84a1e4e38e8d8/patched-fonts/Hack/Italic/complete/Hack%20Italic%20Nerd%20Font%20Complete.ttf
+
+# BoldItalic font
+wget https://github.com/ryanoasis/nerd-fonts/blob/6f0b1fbf4fb03f58015d587c64c84a1e4e38e8d8/patched-fonts/Hack/BoldItalic/complete/Hack%20BoldItalic%20Nerd%20Font%20Complete.ttf
+
