054d55f213cd75b33eb64ddb94168acfd75c2299
reyniersbram <55666730+reyniersbram@users.noreply.github.com>
2023-02-13 23:06:04 +0100

    some changes

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -34,7 +34,7 @@ local options = {
 	wrap = false, -- display lines as one long line
 	scrolloff = 8, -- is one of my fav
     sidescrolloff = 8,
-	guifont = "monospace:h17", -- the font used in graphical neovim applications
+	guifont = "hack:h17", -- the font used in graphical neovim applications
     spelllang = {"en"},
     spell = false, -- enable/disable spellcheck by default
 }
--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -105,12 +105,12 @@ telescope.setup {
              case_mode = "smart_case",        -- or "ignore_case" or "respect_case
                                               -- the default case_mode is "smart_case"
          },
-         media_files = {
-         --     filetypes whitelist ]]
-         --[[     -- defaults to {"png", "jpg", mp4", "webm", "pdf"} ]]
-         --[[     filetype = {"png", "webp", "jpg", "jpeg"}, ]]
-         --[[     find_cmd = "rg" -- find command (defaults to `fd` ]]
-         },
+         --[[ media_files = {
+         --    filetypes whitelist
+         -- defaults to {"png", "jpg", mp4", "webm", "pdf"}
+         filetype = {"png", "webp", "jpg", "jpeg"},
+         find_cmd = "rg" -- find command (defaults to `fd`
+         }, ]]
     },
 }
 
