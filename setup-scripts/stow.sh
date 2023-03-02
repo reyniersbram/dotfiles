434488e8b604dbdd2804ebf45c8d8a47b84c6818
reyniersbram <55666730+reyniersbram@users.noreply.github.com>
2023-03-02 15:56:18 +0100

    [stow] symlink scripts to local/bin

---
+++ setup-scripts/stow.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+CDIR=$1
+
+# Symlink .local/bin to scripts.
+# Has to be done now, because absolute path differs from user to user
+ln -s $CDIR/scripts $CDIR/.local/bin
+
+echo "Symlinking configurations with stow..."
+which stow &> /dev/null
+stow_installed=$?
+if [[ 0 -ne $stow_installed ]]; then
+    echo "stow not installed"
+    echo "Installing stow..."
+    sudo pacman -S stow --noconfirm
+fi
+
+# stow the correct files into the home directory
+stow --verbose=2 --dir $CDIR --target $HOME .
+
