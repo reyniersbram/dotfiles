b95fbb398266558031481162ef5e9168233e53a9
reyniersbram <55666730+reyniersbram@users.noreply.github.com>
2023-05-08 11:09:44 +0200

    [nvim] ignore __pycache__ in telescope

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -11,7 +11,11 @@ telescope.setup {
         prompt_prefix = " ",
         selection_caret = " ",
         path_display = { "smart" },
-        file_ignore_patterns = { "^./.git/", "^./node_modules/" },
+        file_ignore_patterns = {
+            "^./.git/",
+            "^./node_modules/",
+            "^./__pycache__/"
+        },
 
         mappings = {
             i = {
