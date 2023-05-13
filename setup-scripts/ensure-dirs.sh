b6c3825399cfe6c8a8e244c64634454e58796e58
reyniersbram <55666730+reyniersbram@users.noreply.github.com>
2023-05-13 21:07:28 +0200

    #2 required directories

--- setup-scripts/ensure-dirs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p $HOME/.config/
-mkdir -p $HOME/.local/
-mkdir -p $HOME/.local/bin/
-mkdir -p $HOME/.local/share/
-mkdir -p $HOME/Pictures/
-mkdir -p $HOME/Downloads/
-
