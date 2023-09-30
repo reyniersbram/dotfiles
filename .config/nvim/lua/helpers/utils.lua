4e4b11c8eaa4195ebfb7a6e0588ec8733842b3f1
reyniersbram <55666730+reyniersbram@users.noreply.github.com>
2023-09-30 13:02:54 +0200

    [nvim] refactor presence.nvim config

--- .config/nvim/lua/helpers/utils.lua
+++ .config/nvim/lua/helpers/utils.lua
@@ -1,3 +1,27 @@
 local M = {}
 
+M.ssh2http =
+    -- Converts an ssh url, formatted like `git@some.domain:user/repo.git`
+    -- to an http(s) url formatted like `https://some.domain/user/repo` if needed
+    --- @param url string url to be converted
+    --- @return string | nil # the formatted string
+    function(url)
+        if url == nil then
+            return nil
+        end
+        if string.sub(url, 1, 4) == "http" then
+            return url
+        end
+        if string.sub(url, 1, 4) == "git@" then
+            local length = string.len(url)
+            url = "https://" ..
+                string.gsub(
+                    string.sub(url, 5, length - 4),
+                    ":", "/", 1
+                )
+            return url
+        end
+        return nil
+    end
+
 return M
--- .config/nvim/lua/plugins/presence.lua
+++ .config/nvim/lua/plugins/presence.lua
@@ -4,78 +4,63 @@ if not presence_status_ok then
     return
 end
 
--- Converts an ssh url, formatted like `git@some.domain:user/repo.git`
--- to an http(s) url formatted like `https://some.domain/user/repo` if needed
---- @param url string url to be converted
---- @return string # the formatted string
-local function ssh2http(url)
-    if url == nil then
-        return ""
-    end
-    if string.sub(url, 1, 4) == "http" then
-        return url
-    end
-    if string.sub(url, 1, 4) == "git@" then
-        local length = string.len(url)
-        url = "https://" ..
-            string.gsub(
-                string.sub(url, 5, length - 4),
-                ":", "/", 1
-            )
-        return url
-    end
-    return "https://example.org"
-end
+local ssh2http = require("helpers.utils").ssh2http
 
 presence.setup {
     -- General options
 
     -- Update activity based on autocmd events (if `false`, map or manually
     -- execute `:lua package.loaded.presence:update()`)
-    auto_update = true,
+    auto_update         = true,
     -- Text displayed when hovered over the Neovim image
-    neovim_image_text = "The One True Text Editor",
+    neovim_image_text   = "The One True Text Editor",
     -- Main image display (either "neovim" or "file")
-    main_image = "neovim",
+    main_image          = "neovim",
     -- Use your own Discord application client id (not recommended)
-    client_id = "793271441293967371",
+    client_id           = "793271441293967371",
     -- Log messages at or above this level
     -- (one of the following: "debug", "info", "warn", "error")
-    log_level = nil,
+    log_level           = nil,
     -- Number of seconds to debounce events (or calls to
     -- `:lua package.loaded.presence:update(<filename>, true)`)
-    debounce_timeout = 10,
+    debounce_timeout    = 10,
     -- Displays the current line number instead of the current project
-    enable_line_number = false,
+    enable_line_number  = false,
     -- A list of strings or Lua patterns that disable Rich Presence if the
     -- current file name, path, or workspace matches
-    blacklist = {},
+    blacklist           = {},
     -- Configure Rich Presence button(s), either a boolean to enable/disable, a
     -- static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a
     -- function(buffer: string, repo_url: string|nil): table)
-    buttons =
-        function (buffer, repo_url)
-                local excluded = {
-                    "term",
-                }
-                if repo_url == nil then
+    buttons             =
+        function(buffer, repo_url)
+            if repo_url == nil then
+                return false
+            end
+            local excluded = {
+                "term",
+            }
+            for _, item in pairs(excluded) do
+                if string.match(repo_url, item) then
                     return false
                 end
-                if string.match(repo_url, "term") then
-                    return false
-                end
-                return {
-                    {
-                        label = "View Repository",
-                        url = ssh2http(repo_url),
-                    },
-                }
+            end
+            repo_url = ssh2http(repo_url)
+            if repo_url == nil then
+                return false
+            end
+            return {
+                {
+                    label = "View Repository",
+                    url = repo_url,
+                },
+            }
         end,
     -- Custom file asset definitions keyed by file names and extensions (see
     -- default config at `lua/presence/file_assets.lua` for reference)
-    file_assets = {},
+    file_assets         = {},
     -- Show the timer
-    show_time = true,
+    show_time           = true,
 
     -- Rich Presence text options
     editing_text        = "Editing %s",         -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
