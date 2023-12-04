ae616b6cf2c2a6139acdabd9d0094cf14dd85778
reyniersbram <55666730+reyniersbram@users.noreply.github.com>
2023-12-04 12:26:48 +0100

    update screen notification script

--- .local/bin/break-reminder.sh
+++ .local/bin/break-reminder.sh
@@ -4,6 +4,7 @@ export XAUTHORITY=/home/reyniersbram/.Xauthority
 export DISPLAY=:0
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 
-notify-send "Rest your eyes" "Relax your eyes. Take a look outside for a minute."\
+notify-send "Relax your eyes. Take a look outside for a minute." ""\
     -i "${HOME}/Pictures/icons/glasses/glasses.svg"\
-    -h string:x-dunst-stack-tag:break-reminder
+    -h string:x-dunst-stack-tag:break-reminder\
+    --app-name "Rest your eyes"
