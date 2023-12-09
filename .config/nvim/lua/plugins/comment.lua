2da6d3eb1e8d945b953f9533b2182f577e8e6e9e
reyniersbram <55666730+reyniersbram@users.noreply.github.com>
2023-12-09 12:31:59 +0100

    [nvim] some treesitter configuring

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -1,9 +1,9 @@
 local require = require("helpers.utils").cb_require
 
 return {
-    "numToStr/Comment.nvim",
-    dependencies = {
-        "JoosepAlviste/nvim-ts-context-commentstring",
-    },
-    config = require("plugins.config.comment"),
+    -- "numToStr/Comment.nvim",
+    -- dependencies = {
+    --     "JoosepAlviste/nvim-ts-context-commentstring",
+    -- },
+    -- config = require("plugins.config.comment"),
 }
--- .config/nvim/lua/plugins/config/treesitter.lua
+++ .config/nvim/lua/plugins/config/treesitter.lua
@@ -4,20 +4,63 @@ if not ts_status_ok then
     return
 end
 
+local parser_dir = vim.fn.stdpath("data") .. "/treesitter"
+vim.opt.rtp:append(parser_dir)
+
 treesitter.setup {
-    ensure_installed = "all",
-    sync_install = false,
-    ignore_install = { "" }, -- List of parsers to ignore installing
-    autopairs = {
-        enable = true,
+    parser_install_dir = parser_dir,
+    ensure_installed = {
+        "arduino",
+        "bash",
+        "c", "cmake", "cpp",
+        "css", "scss",
+        "html", "vue",
+        "javascript", "typescript", "tsx", "jsdoc",
+        "json", "jsonc", "json5", "jsonnet",
+        "http", "sparql", "turtle",
+        "terraform",
+        "csv", "psv", "tsv",
+        "diff",
+        "make", "dockerfile", "nix",
+        "dot",
+        "git_config", "git_rebase", "gitattributes", "gitcommit", "gitignore",
+        "gpg",
+        "haskell", "haskell_persistent",
+        "ini", "toml", "xml", "yaml",
+        "java", "kotlin", "properties",
+        "latex", "markdown", "markdown_inline",
+        "lua", "luadoc", "luap", "luau",
+        "nasm",
+        "proto",
+        "python", "requirements",
+        "query",
+        "regex",
+        "sql",
+        "ssh_config",
+        "vim", "vimdoc",
     },
+    ignore_install = {},
+    sync_install = false,
+    auto_install = false,
     highlight = {
-        enable = true, -- false will disable the whole extension
-        disable = { "" }, -- list of language that will be disabled
+        enable = true,
+        disable = {},
         additional_vim_regex_highlighting = false,
     },
+    -- incremental_selection = {
+    --     enable = false,
+    --     keymaps = {
+    --         init_selection = "gnn",
+    --         node_incremental = "grn",
+    --         scope_incremental = "grc",
+    --         node_decremental = "grm",
+    --     },
+    -- },
     indent = {
         enable = true,
-        disable = { "" }
+        disable = {},
     },
+    -- autopairs = {
+    --     enable = true,
+    -- },
  }
