be0b4bd15bbefc9e5af718da1562efdc6f19ef45
reyniersbram <55666730+reyniersbram@users.noreply.github.com>
2023-12-09 12:29:49 +0100

    [nvim] set space as leader key in core.keymaps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,14 +1,5 @@
 require "core"
 
--- Remap space as leader key
--- Should happen before lazy.setup
-vim.api.nvim_set_keymap(
-    "", "<Space>", "<Nop>",
-    { noremap = true, silent = true }
-)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
 require "lazy-setup"
 vim.notify = require("notify")
 
--- .config/nvim/lua/core/keymap.lua
+++ .config/nvim/lua/core/keymap.lua
@@ -1,6 +1,13 @@
 local opts = { noremap = true, silent = true }
-
 local keymap = vim.api.nvim_set_keymap
+
+-- Remap space as leader key
+keymap(
+    "", "<Space>", "<Nop>",
+    { noremap = true, silent = true }
+)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Modes
 --   normal_mode = "n",
