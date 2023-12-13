3d277725b70c769be11bd7c6c8a13d5fb9aef0bf
reyniersbram <55666730+reyniersbram@users.noreply.github.com>
2023-12-13 20:31:53 +0100

    [nvim] format utils.lua

--- .config/nvim/lua/helpers/utils.lua
+++ .config/nvim/lua/helpers/utils.lua
@@ -1,28 +1,27 @@
 local M = {}
 
-M.ssh2http =
-    -- Converts an ssh url, formatted like `git@some.domain:user/repo.git`
-    -- to an http(s) url formatted like `https://some.domain/user/repo` if needed
-    --- @param url string url to be converted
-    --- @return string | nil # the formatted string
-    function(url)
-        if url == nil then
-            return nil
-        end
-        if string.sub(url, 1, 4) == "http" then
-            return url
-        end
-        if string.sub(url, 1, 4) == "git@" then
-            local length = string.len(url)
-            url = "https://" ..
-                string.gsub(
-                    string.sub(url, 5, length - 4),
-                    ":", "/", 1
-                )
-            return url
-        end
+--- Converts an ssh url, formatted like `git@some.domain:user/repo.git`
+--- to an http(s) url formatted like `https://some.domain/user/repo` if needed
+--- @param url string
+--- @return string | nil
+function M.ssh2http(url)
+    if url == nil then
         return nil
     end
+    if string.sub(url, 1, 4) == "http" then
+        return url
+    end
+    if string.sub(url, 1, 4) == "git@" then
+        local length = string.len(url)
+        url = "https://" ..
+            string.gsub(
+                string.sub(url, 5, length - 4),
+                ":", "/", 1
+            )
+        return url
+    end
+    return nil
+end
 
 --- Same as require, but returns a callback to require a certain module
 --- @param modname string
