7aa04220e2c5a8e6f4ed5bb3c9c5b421c3002b34
reyniersbram <55666730+reyniersbram@users.noreply.github.com>
2023-12-28 00:43:53 +0100

    [nvim] add todo for interesting git plugins

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,5 +1,8 @@
 local require = require("util").cb_require
 return {
-    "lewis6991/gitsigns.nvim",
-    config = require("plugins.config.gitsigns")
+    {
+        "lewis6991/gitsigns.nvim",
+        config = require("plugins.config.gitsigns"),
+    },
+    -- TODO: check lazygit.nvim and octo.nvim
 }
