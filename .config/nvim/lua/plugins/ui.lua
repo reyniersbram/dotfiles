479a815254e1e49b1a86355a430a99e1a3af33b0
reyniersbram <55666730+reyniersbram@users.noreply.github.com>
2024-01-27 23:28:08 +0100

    [nvim] installed neoscroll

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -10,4 +10,21 @@ return {
         "nvim-tree/nvim-web-devicons",
         lazy = true,
     },
+    {
+        "karb94/neoscroll.nvim",
+        opts = {
+            mappings = {
+                "<C-u>", "<C-d>", "<C-b>", "<C-f>",
+                "<C-y>", "<C-e>", "zt", "zz", "zb"
+            },
+            hide_cursor = true,
+            stop_eof = true,
+            respect_scrolloff = false,
+            cursor_scrolls_alone = true,
+            easing_function = "sine",
+            pre_hook = nil,
+            post_hook = nil,
+            performance_mode = false,
+        },
+    },
 }
