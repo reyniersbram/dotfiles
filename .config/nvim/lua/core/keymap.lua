f4e77f16972bd74c638f466b6f9b6b0b98ae6b50
reyniersbram <55666730+reyniersbram@users.noreply.github.com>
2024-01-29 22:00:40 +0100

    [nvim] big core keymap refactor

--- .config/nvim/lua/core/keymap.lua
+++ .config/nvim/lua/core/keymap.lua
@@ -1,56 +1,43 @@
-local opts = { noremap = true, silent = true }
-local keymap = vim.api.nvim_set_keymap
+local default_opts = { noremap = true, silent = true }
+
+---@param mode string|table<string> See :help modes for available modes
+---@param lhs string
+---@param rhs string|fun()
+---@param custom_opts? table Optional, if not specified default values are used
+---     See :help vim.keymap.set for possible values
+---     Default values: { noremap = true, silent = true }
+local function keymap(mode, lhs, rhs, custom_opts)
+    local opts = vim.tbl_deep_extend("force", default_opts, custom_opts or {})
+    vim.keymap.set(mode, lhs, rhs, opts)
+end
 
 -- Remap space as leader key
-keymap(
-    "", "<Space>", "<Nop>",
-    { noremap = true, silent = true }
-)
+keymap("", "<Space>", "<Nop>", { desc = "Space does nothing" })
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Modes
---   normal_mode = "n",
---   insert_mode = "i",
---   visual_mode = "v",
---   visual_block_mode = "x",
---   term_mode = "t",
---   command_mode = "c",
+keymap("n", "<C-h>", "<cmd>wincmd h<CR>", { desc = "Focus window to the left" })
+keymap("n", "<C-j>", "<cmd>wincmd j<CR>", { desc = "Focus window below" })
+keymap("n", "<C-k>", "<cmd>wincmd k<CR>", { desc = "Focus window above" })
+keymap("n", "<C-l>", "<cmd>wincmd l<CR>", { desc = "Focus window to the right" })
 
--- Normal --
--- Window navigation
-keymap("n", "<C-h>", ":wincmd h<CR>", opts)
-keymap("n", "<C-j>", ":wincmd j<CR>", opts)
-keymap("n", "<C-k>", ":wincmd k<CR>", opts)
-keymap("n", "<C-l>", ":wincmd l<CR>", opts)
+keymap("n", "<C-Up>", "<cmd>resize +2<CR>", { desc = "Increase window height" })
+keymap("n", "<C-Down>", "<cmd>resize -2<CR>", { desc = "Decrease window height" })
+keymap("n", "<C-Left>", "<cmd>vertical resize -2<CR>", { desc = "Decrease window width" })
+keymap("n", "<C-Right>", "<cmd>vertical resize +2<CR>", { desc = "Increase window width" })
 
--- Resize with arrows
-keymap("n", "<C-Up>", ":resize +2<CR>", opts)
-keymap("n", "<C-Down>", ":resize -2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap("n", "<leader>w", "<cmd>w<CR>", { desc = "Save buffer" })
+keymap("n", "<leader>q", "<cmd>q<CR>", { desc = "Close window" })
+keymap("n", "<leader>Q", "<cmd>wqa<CR>", { desc = "Save all buffers and quit Neovim" })
 
--- Simple closing
-keymap("n", "<leader>w", ":w<CR>", opts)
-keymap("n", "<leader>q", ":q<CR>", opts)
-keymap("n", "<leader>Q", ":wqa<CR>", opts)
+keymap("n", "<leader>y", "<cmd>%y+<CR>", { desc = "Copy whole buffer" })
 
--- Copy Whole buffer
-keymap("n", "<leader>y", ":%y+<CR>", opts)
+keymap("v", "<", "<gv", { desc = "Keep selection selected after indenting" })
+keymap("v", ">", ">gv", { desc = "Keep selection selected after indenting" })
 
--- Visual --
--- Stay in indent mode
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+keymap("v", "J", "<cmd>move '>+1<CR>gv=gv", { desc = "Move line down, fix indentation" })
+keymap("v", "K", "<cmd>move '>-2<CR>gv=gv", { desc = "Move line up, fix indentation" })
+keymap("v", "<A-j>", "<cmd>move '>+1<CR>gv-gv", { desc = "Move line down, don't fix indentation" })
+keymap("v", "<A-k>", "<cmd>move '<-2<CR>gv-gv", { desc = "Move line up, don't fix indentation" })
 
--- Move text up and down
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "p", '"_dP', opts)
-
--- Visual Block --
--- Move text up and down
-keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+keymap("v", "p", '"_dP', { desc = "Paste and keep in register" })
