f05e7d718ad65818ae10a322bb40d94e42e818b4
Bram Reyniers <reyniersbram@gmail.com>
2025-05-08 10:19:20 +0200

    [nvim] put lsp configuration in lsp rtp

--- .config/nvim/lua/core/lsp/init.lua
+++ .config/nvim/lua/core/lsp/init.lua
@@ -1,13 +1,5 @@
-vim.lsp.config('lua_ls', {
-    cmd = { "lua-language-server" },
-    root_markers = {
-        ".luarc.json", ".luarc.jsonc",
-        ".luacheckrc",
-        ".stylua.toml", "stylua.toml",
-        "selene.toml", "selene.yml",
-        ".git"
-    },
-    filetypes = { "lua" },
+vim.lsp.config("*", {
+    root_markers = { ".git" },
 })
 
 vim.lsp.enable("lua_ls")
