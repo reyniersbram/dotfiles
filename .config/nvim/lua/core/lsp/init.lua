d0ca15da710be816f2b9c143fdf2d97fba8cfbf8
Bram Reyniers <reyniersbram@gmail.com>
2025-07-02 08:47:17 +0200

    [nvim] enable inlay hints

--- .config/nvim/lua/core/lsp/init.lua
+++ .config/nvim/lua/core/lsp/init.lua
@@ -13,4 +13,14 @@ vim.lsp.enable({
     "vue_ls", -- vue
 })
 
+vim.api.nvim_create_autocmd("LSPAttach", {
+    group = vim.api.nvim_create_augroup("my.lsp", {}),
+    callback = function (args)
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+        if client:supports_method("textDocument/inlayHint") then
+            vim.lsp.inlay_hint.enable(true, {bufnr = args.buf})
+        end
+    end,
+})
+
 require("core.lsp.document_highlight").setup({ enable = true })
