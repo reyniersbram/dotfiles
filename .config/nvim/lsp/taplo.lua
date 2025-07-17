951ce1cbccd0f8c404fdb1363100765856855c24
Bram Reyniers <reyniersbram@gmail.com>
2025-07-17 10:17:39 +0200

    [nvim] configure taplo lsp

--- .config/nvim/lsp/taplo.lua
+++ .config/nvim/lsp/taplo.lua
@@ -1,5 +1,30 @@
 return {
-  cmd = { 'taplo', 'lsp', 'stdio' },
-  filetypes = { 'toml' },
-  root_markers = { '.taplo.toml', 'taplo.toml', '.git' },
+    cmd = { "taplo", "lsp", "stdio" },
+    filetypes = { "toml" },
+    root_markers = { ".taplo.toml", "taplo.toml", ".git" },
+    settings = {
+        evenBetterToml = {
+            formatter = {
+                alignEntries = true,
+                alignComments = true,
+                arrayTrailingComma = true,
+                arrayAutoExpand = true,
+                arrayAutoCollapse = true,
+                compactArrays = true,
+                compactInlineTables = false,
+                inlineTableExpand = true,
+                compactEntries = false,
+                columnWidth = 80,
+                indentTables = false,
+                indentEntries = false,
+                indentString = "    ",
+                trailingNewline = true,
+                reorderKeys = false,
+                reorderArrays = true,
+                reorderInlineTables = false,
+                allowedBlankLines = 2,
+                crlf = false,
+            },
+        },
+    },
 }
