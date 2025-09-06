d5928c9d84302b81030fabe0dd04627240cdd9ea
Bram Reyniers <reyniersbram@gmail.com>
2025-09-06 11:51:21 +0200

    feat(gnupg): add install script for gnupg

---
+++ install.d/gnupg.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+set -eu
+
+current_directory=$(cd -- "$(dirname -- "$0")" > /dev/null 2>&1 && pwd)
+. "${current_directory}/../lib.sh"
+
+log "Installing GnuPG..." info
+install_packages gnupg
+
+log "Linking configuration..." info
+ln --symbolic --force --no-target-directory --verbose \
+    "${current_directory}/../.gnupg/gpg-agent.conf" \
+    "${HOME}/.gnupg/gpg-agent.conf"
+
+log "Done." info
