817c7ab3602719af3d8167ad81ea3f9c6e0230ae
Bram Reyniers <reyniersbram@gmail.com>
2025-10-14 21:29:26 +0200

    feat(x11): handle xbindkeys in x11 install script

--- install.d/x11.sh
+++ install.d/x11.sh
@@ -6,7 +6,7 @@ current_directory=$(cd -- "$(dirname -- "$0")" > /dev/null 2>&1 && pwd)
 . "${current_directory}/../lib.sh"
 
 log "Installing packages for X11..." info
-install_packages xorg-server xorg-xinit xorg-xrdb xorg-xrandr
+install_packages xorg-server xorg-xinit xorg-xrdb xorg-xrandr xbindkeys
 
 log "Linking configuration..." info
 ln --symbolic --force --no-target-directory --verbose \
@@ -19,3 +19,9 @@ mkdir --parents --verbose "$XDG_CONFIG_HOME"
 ln --symbolic --force --no-target-directory --verbose \
     "$(realpath "${current_directory}/../.config/Xresources")" \
     "${XDG_CONFIG_HOME}/Xresources"
+
+# xbindkeys
+ln --symbolic --force --no-target-directory --verbose \
+    "$(realpath "${current_directory}/../.xbindkeysrc")" \
+    "$HOME/.xbindkeysrc"
+
