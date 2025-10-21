64744b94c0260821fc9dddb3f080a3db92985ebb
Bram Reyniers <reyniersbram@gmail.com>
2025-10-21 11:32:21 +0200

    feat(font): add install script for fonts

---
+++ install.d/font.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+set -eu
+
+current_directory=$(cd -- "$(dirname -- "$0")" > /dev/null 2>&1 && pwd)
+. "${current_directory}/../lib.sh"
+
+name=fontconfig
+
+: "${XDG_CONFIG_HOME:=$HOME/.config}"
+config_target="${XDG_CONFIG_HOME}/${name}"
+
+log "Installing font configuration..." info
+install_packages fontconfig ttf-hack-nerd
+
+log "Linking configuration..." info
+mkdir --parents --verbose "$XDG_CONFIG_HOME"
+ln --symbolic --force --no-target-directory --verbose \
+    "$(realpath "${current_directory}/../.config/${name}")" \
+    "$config_target"
+
+log "Done." info
