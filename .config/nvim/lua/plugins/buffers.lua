da1f4e60a4895c65104260b50bf81fb5daeb011a
Bram Reyniers <reyniersbram@gmail.com>
2026-02-09 20:48:13 +0100

    feat(nvim): latex writing and vim-bbye plugin

---
+++ .config/nvim/lua/plugins/buffers.lua
@@ -0,0 +1,4 @@
+---@type LazySpec
+return {
+    "moll/vim-bbye"
+}
---
+++ .config/nvim/lua/plugins/latex.lua
@@ -0,0 +1,8 @@
+--- @type LazySpec
+return {
+    "lervag/vimtex",
+    init = function()
+        vim.g.vimtex_view_method = "zathura"
+        vim.g.vimtex_compiler_method = "latexmk"
+    end
+}
