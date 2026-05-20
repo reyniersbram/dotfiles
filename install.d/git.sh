2b2d3b55123d814b4726ceb28e8214728dde5236
Bram Reyniers <reyniersbram@gmail.com>
2026-05-20 22:48:16 +0200

    feat(git): add git fixup command

--- install.d/git.sh
+++ install.d/git.sh
@@ -9,6 +9,7 @@ name=git
 
 : "${XDG_CONFIG_HOME:=$HOME/.config}"
 config_target="${XDG_CONFIG_HOME}/${name}"
+bin_target="${HOME}/.local/bin"
 
 log "Installing git..." info
 install_packages git
@@ -18,5 +19,8 @@ mkdir --parents --verbose "$XDG_CONFIG_HOME"
 ln --symbolic --force --no-target-directory --verbose \
     "$(realpath "${current_directory}/../.config/${name}")" \
     "$config_target"
+ln --symbolic --force --no-target-directory --verbose \
+    "$(realpath "${current_directory}/../.local/bin/git-fixup")" \
+    "${bin_target}/git-fixup"
 
 log "Done." info
